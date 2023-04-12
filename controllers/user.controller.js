1d90bba5fe0baaf9496b88e1091ad46a843d4332
NeRpi <73479715+NeRpi@users.noreply.github.com>
2023-04-12 20:13:35 +0300

    Add bcryptjs

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,6 +2,7 @@ const User = require('../models/user.model');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const saltRounds = 10;
 
 class UserController {
     constructor(sequelize) {
@@ -11,17 +12,17 @@ class UserController {
 
     signUp = async (req, res) => {
         try {
-            const user = await this.models.User.create(req.body);
+            const { user_name, password } = req.body;
+            const passwordHash = await bcrypt.hash(password, saltRounds);
+            const user = await this.models.User.create({ user_name, password: passwordHash });
             const token = jwt.sign(
                 { user_id: user._id, user_name: user.user_name },
                 process.env.TOKEN_KEY,
                 { expiresIn: '2h' }
             );
 
-            console.log('Регистрация пользователя');
             res.status(201).json({ accessToken: token });
         } catch (err) {
-            console.log('Произошла ошибка при регистрации польщователя: ', err);
             res.send(err);
         }
     }
@@ -29,21 +30,23 @@ class UserController {
     login = async (req, res) => {
         try {
             const { user_name, password } = req.body;
-            const user = await this.models.User.findOne({ user_name });
-            if (user && password == user.password) {
-                const token = jwt.sign(
-                    { user_id: user._id, user_name: user.user_name },
-                    process.env.TOKEN_KEY,
-                    { expiresIn: "2h", }
-                );
-
-                console.log('Вход пользователя пользователя');
-                res.status(200).json({ accessToken: token });
+            const user = await this.models.User.findOne({ where: { user_name } });
+            if (user) {
+                const passwordMatch = await bcrypt.compare(password, user.password);
+                if (passwordMatch) {
+                    const token = jwt.sign(
+                        { user_id: user._id, user_name: user.user_name },
+                        process.env.TOKEN_KEY,
+                        { expiresIn: "2h", }
+                    );
+                    res.status(200).json({ accessToken: token });
+                } else {
+                    res.status(404).json('Не верный пароль');
+                }
             } else {
-                res.status(403).json({ message: 'Не верно введён пароль или имя пользователя' });
+                res.status(403).json({ message: 'Пользователь не найден' });
             }
         } catch (err) {
-            console.log('Произошла ошибка при регистрации польщователя: ', err);
             res.send(err);
         }
     }
